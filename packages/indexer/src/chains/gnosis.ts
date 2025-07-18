3700c8f7aaaaf4f8b07f9e0e834dafae98476326
Dmitry <dmitry@goverland.xyz>
2025-07-18 16:49:22 +0300

    Preparations for Dockerization

--- packages/indexer/src/chains/gnosis.ts
+++ packages/indexer/src/chains/gnosis.ts
@@ -2,7 +2,7 @@ import { ChainConfig } from './types'
 
 export default {
   gateway: 'https://v2.archive.subsquid.io/network/gnosis-mainnet',
-  rpc: 'https://airlock.gnosisguild.org/api/v1/100/rpc',
+  rpc: process.env.GNOSIS_RPC_URL || 'https://airlock.gnosisguild.org/api/v1/100/rpc',
   chainId: 100,
   shortName: 'gno',
   finality: 20,
